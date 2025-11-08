cdb9f0f7162ce3e3888e42527d1c6ce51ce9ce65
Zang Bin <zangbin36@gmail.com>
2025-11-08 23:45:28 +0800

    terminal open in insert mode

--- init.lua
+++ init.lua
@@ -228,6 +228,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('TermOpen', {
+  desc = 'Enters terminal in insert mode',
+  group = vim.api.nvim_create_augroup('kickstart-term-open', { clear = true }),
+  pattern = '*',
+  command = 'startinsert',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
